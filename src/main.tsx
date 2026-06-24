98a0182b6098d4433a265bf831c78c925676ea74
nithin cv <nithincvpoyyil@users.noreply.github.com>
2026-06-25 00:31:33 +1000

    feat: reorganize styles into separate CSS files and update imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
+import './styles/index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
